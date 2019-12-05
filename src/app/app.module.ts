39d3db6e96350e5e6e10b779b685dd4504266d5e
omerozoglu <omerozoglu.1@gmail.com>
2019-12-05 16:19:17 +0300

    Add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ProductComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule

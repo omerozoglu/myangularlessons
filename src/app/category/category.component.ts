aaaea8ab4e7e3c7ab087d27c985b9a5e88985354
omerozoglu <omerozoglu.1@gmail.com>
2019-12-05 17:57:52 +0300

    Category List

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './category';
 
 @Component({
   selector: 'app-category',
@@ -8,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   constructor() { }
+  title="Category List"
+  categories :Category[]=[
+    {
+      "id":1,
+      "name":"Phone"
+    },
+    {
+      "id":2,
+      "name":"Mouse"
+    },
+    {
+      "id":3,
+      "name":"PC"
+    },
+    {
+      "id":4,
+      "name":"Watch"
+    }
 
+  ]
   ngOnInit() {
   }
 

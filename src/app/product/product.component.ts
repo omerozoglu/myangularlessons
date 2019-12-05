a051023faa66d09f3fed372a8b1f101e7bf3c367
omerozoglu <omerozoglu.1@gmail.com>
2019-12-05 17:24:11 +0300

    ng_for

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,7 +9,45 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title="Product List"
-  products :  any[]
+  products :  any[]=[
+    { 
+      id:1,
+      name:"Phone",
+      price:2000,
+      categoryId:1,
+      description:"Sony Xperia XZ1 Phone"
+    },{ 
+      id:2,
+      name:"Mouse",
+      price:30,
+      categoryId:2,
+      description:"Bloody V7MA core4 Mouse"
+    },{ 
+      id:1,
+      name:"Phone",
+      price:2000,
+      categoryId:1,
+      description:"Sony Xperia XZ1 Phone"
+    },{ 
+      id:2,
+      name:"Mouse",
+      price:30,
+      categoryId:2,
+      description:"Bloody V7MA core4 Mouse"
+    },{ 
+      id:1,
+      name:"Phone",
+      price:2000,
+      categoryId:1,
+      description:"Sony Xperia XZ1 Phone"
+    },{ 
+      id:2,
+      name:"Mouse",
+      price:30,
+      categoryId:2,
+      description:"Bloody V7MA core4 Mouse"
+    }
+  ]
   
   ngOnInit() {
   }
